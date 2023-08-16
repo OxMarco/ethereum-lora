23fac69b09b7de6ebb8a2cb4c0b967d1f37f09b7
OxMarco <>
2023-08-16 13:29:28 +0200

    fix

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from lora.lora_e22 import LoRaE22, Configuration
 from lora.lora_e22_operation_constant import ResponseStatusCode
 from lora.lora_e22_constants import FixedTransmission, RssiEnableByte
 
+
 def test_connection():
-    print("Testing connection to Geth node...", end='', flush=True)
+    print("Testing connection to the node...", end='', flush=True)
 
     method = "web3_clientVersion"
     payload = {
@@ -17,7 +18,7 @@ def test_connection():
         "params": [],
         "id": 1,
     }
-    response = send_to_geth(json.dumps(payload))
+    response = send_to_node(json.dumps(payload))
     if response["success"]:
         print("\nConnection successful!")
         print("Client Version:", response["data"]["result"])
@@ -27,68 +28,65 @@ def test_connection():
         exit(1)
 
 
-def send_to_geth(data):
-    url = os.environ.get('NODE_URL', 'http://127.0.0.1:8545') # Default URL if the environment variable is not set
-
+def send_to_node(data):
+    url = os.environ.get('NODE_URL', 'http://127.0.0.1:8545')
     headers = {'Content-type': 'application/json'}
 
-    try:
-        response = requests.post(url, data=data, headers=headers)
-        response.raise_for_status()
+    response = requests.post(url, data=data, headers=headers)
+    response.raise_for_status()
 
-        return {"success": True, "data": response.json()}
-    except requests.exceptions.RequestException as e:
-        return {"success": False, "error": str(e)}
+    return {"success": True, "data": response.json()}
 
 
 def main():
-    test_connection()
+    try:
+        test_connection()
 
-    lora_chip_model = os.environ.get('LORA_CHIP_MODEL', '400T33D')
-    serial_port = os.environ.get('SERIAL_PORT', '/dev/serial0')
-    aux_pin = int(os.environ.get('AUX_PIN', 18))
-    m0_pin = int(os.environ.get('M0_PIN', 23))
-    m1_pin = int(os.environ.get('M1_PIN', 24))
+        lora_chip_model = os.environ.get('LORA_CHIP_MODEL', '400T33D')
+        serial_port = os.environ.get('SERIAL_PORT', '/dev/serial0')
+        aux_pin = int(os.environ.get('AUX_PIN', 18))
+        m0_pin = int(os.environ.get('M0_PIN', 23))
+        m1_pin = int(os.environ.get('M1_PIN', 24))
 
-    print("LoRa E22 setup...")
-    loraSerial = serial.Serial(serial_port)
-    lora = LoRaE22(lora_chip_model, loraSerial, aux_pin=aux_pin, m0_pin=m0_pin, m1_pin=m1_pin)
-    code = lora.begin()
-    if code != 1:
-        print("LoRa interfacing error!")
-        return
-    
-    configuration_to_set = Configuration(lora_chip_model)
-    # To enable RSSI, you must also enable RSSI on receiver
-    configuration_to_set.TRANSMISSION_MODE.enableRSSI = RssiEnableByte.RSSI_ENABLED
-    code, confSetted = lora.set_configuration(configuration_to_set)
+        print("LoRa E22 setup...")
+        lora_serial = serial.Serial(serial_port)
+        lora = LoRaE22(lora_chip_model, lora_serial, aux_pin=aux_pin, m0_pin=m0_pin, m1_pin=m1_pin)
+        code = lora.begin()
+        if code != 1:
+            print("LoRa interfacing error!")
+            return
 
-    if code != 1:
-        print("LoRa setup error!")
-        return
-    print("OK")
+        configuration_to_set = Configuration(lora_chip_model)
+        configuration_to_set.TRANSMISSION_MODE.enableRSSI = RssiEnableByte.RSSI_ENABLED
+        code, conf_setted = lora.set_configuration(configuration_to_set)
 
-    print("Listener started...")
-    print("\n------------------\n")
-    while True:
-        if lora.available() > 0:
+        if code != 1:
+            print("LoRa setup error!")
+            return
+        print("OK")
 
-            try:
-                code, value, rssi = lora.receive_message(rssi=True)
-                print("Received a new message")
-                print(value)
-                print("RSSI", rssi)
+        print("Listener started...")
+        print("\n------------------\n")
+        while True:
+            if lora.available() > 0:
+                try:
+                    code, value, rssi = lora.receive_message(rssi=True)
+                    print("Received a new message")
+                    print(value)
+                    print("RSSI", rssi)
 
-                print("Responding...")
-                response = send_to_geth(value)
-                print(json.dumps(response, indent=4))
-                code = lora.send_transparent_message(response)
-                if code != 1:
-                    print("Error!")
-                print("OK")
-            except Exception as e:
-                print(json.dumps({"success": False, "error": f"An unexpected error occurred: {e}"}, indent=4))
-                continue
+                    print("Responding...")
+                    response = send_to_node(value)
+                    print(json.dumps(response, indent=4))
+                    #code = lora.send_transparent_message(response)
+                    #if code != 1:
+                    #    print("Error!")
+                    print("OK")
+                except Exception as e:
+                    print(json.dumps({"success": False, "error": f"An unexpected error occurred: {e}"}, indent=4))
+                    continue
+    except requests.exceptions.RequestException as e:
+        print(f"Connection error: {e}")
 
 
 if __name__ == "__main__":
